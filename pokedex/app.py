a5f8abeacfc3660b7994d624c55e7e3c730d8f77
Murilo Carlos <efgs96@gmail.com>
2020-06-06 11:26:39 -0300

    Update app.py

--- pokedex/app.py
+++ pokedex/app.py
@@ -13,16 +13,6 @@ def home():
     poke = res.json()
 
     return render_template('index.html', pokemon=pokemon, poke=poke, title='Flask Pokedex')
-
-@app.route('/pokemons/<int:id>', methods=['GET'])
-def info(id):
-    r = requests.get('https://pokeapi.co/api/v2/pokedex/2')
-    pokemon = r.json()
-
-    res = requests.get('https://pokeapi.co/api/v2/pokemon/{}'.format(id))
-    poke = res.json()
-
-    return render_template('info.html', poke=poke, pokemon=pokemon ,title='Flask Pokedex')
     
 if __name__ == '__main__':
     app.run(debug=True)
